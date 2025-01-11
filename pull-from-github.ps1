b08355acf6425832e125d7f951e24450e51e2977
Joseph <joe@newsunseo.com>
2025-01-11 08:24:21 -0500

    Auto-commit on 2025-01-11 08:24:21

--- pull-from-github.ps1
+++ pull-from-github.ps1
@@ -1,7 +1,6 @@
 # Navigate to the project directory
 cd D:\__MEMECOIN_SCANNER\_memecoin_scanner
 
-<<<<<<< HEAD
 # Check if the remote repository is configured
 $remote = git remote -v
 
@@ -10,7 +9,5 @@ if (-not $remote) {
     git remote add origin https://github.com/NewSunSEO/__MEMECOIN_SCANNER.git
 }
 
-=======
->>>>>>> 74a97ed3a6c983db4ac8470422d04defb8daa0b7
 # Pull the latest changes from the remote repository
 git pull origin main
