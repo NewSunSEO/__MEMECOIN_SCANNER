09b1b957d6f14a56e6e5fd59e336e3df51b17caf
Joseph <joe@newsunseo.com>
2025-01-11 08:08:57 -0500

    Auto-commit on 2025-01-11 08:08:57

--- push-to-github.ps1
+++ push-to-github.ps1
@@ -1,7 +1,6 @@
 # Navigate to the project directory
 cd D:\__MEMECOIN_SCANNER\_memecoin_scanner
 
-<<<<<<< HEAD
 # Check if the remote repository is configured
 $remote = git remote -v
 
@@ -10,8 +9,6 @@ if (-not $remote) {
     git remote add origin https://github.com/NewSunSEO/__MEMECOIN_SCANNER.git
 }
 
-=======
->>>>>>> 74a97ed3a6c983db4ac8470422d04defb8daa0b7
 # Add all changes to staging
 git add .
 
